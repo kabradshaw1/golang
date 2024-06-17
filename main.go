118cbdb9e500dd6c82e678d5e6dffa57915543fc
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-16 21:06:36 -0400

    working on the things

--- main.go
+++ main.go
@@ -1,36 +1,86 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
-type MySlowReader struct {
-	contents string
-	pos      int
+type Page struct {
+	Name string `json:"name"`
 }
 
-func (m MySlowReader) Read(p []byte) (n int, err error) {
-	if m.pos+1 <= len(m.contents) {
-		n := copy(p, m.contents[m.pos:m.pos+1])
-		m.pos++
-		return n, nil
-	}
-	return 0, io.EOF
+type Words struct {
+	Input string   `json:"input"`
+	Words []string `json:"words"`
+}
+
+type Occurence struct {
+	Words map[string]int `json:"words"`
 }
 
 func main() {
+	args := os.Args
 
-	mySlowerReaderInstance := MySlowReader{
-		contents: "Hello World",
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./http-get <url>\n")
+		os.Exit(1)
 	}
 
-	out, err := io.ReadAll(mySlowerReaderInstance)
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("Usage: ./http-get <url>\n\nURL is not valid URL: %s\n", args[1])
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("output: %s\n", out)
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if response.StatusCode != 200 {
+		fmt.Printf("Invalid output (HTTP Code %d): %s\n", response.StatusCode, string(body))
+		os.Exit(1)
+	}
+	var page Page
+
+	err = json.Unmarshal(body, &page)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch page.Name {
+	case "words":
+
+		var words Words
+
+		err = json.Unmarshal(body, &words)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("JSON: Parsed:\nPage: %s\nWords: %s\n", page.Name, strings.Join(words.Words, ", "))
+	case "occurence":
+		var occurence Occurence
+
+		err = json.Unmarshal(body, &occurence)
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		fmt.Printf("Page not found\n")
+	}
+
 }
