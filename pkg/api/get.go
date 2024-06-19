68b71eb4e9d1d2dbbd2744115a15351f3a9b3d45
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-18 23:08:39 -0400

    git ma learn on

--- pkg/api/get.go
+++ pkg/api/get.go
@@ -1,13 +1,10 @@
-package main
+package api
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
-	"os"
 	"strings"
 )
 
@@ -38,57 +35,6 @@ func (o Occurrence) GetResponse() string {
 		words = append(words, fmt.Sprintf("%s (%d)", word, occurrence))
 	}
 	return fmt.Sprintf("Words: %s", strings.Join(words, ", "))
-}
-
-func main() {
-	var (
-		requestURL string
-		password   string
-		parsedURL  *url.URL
-		err        error
-	)
-	flag.StringVar(&requestURL, "url", "", "url to access")
-	flag.StringVar(&password, "password", "", "use a password to access our api")
-
-	flag.Parse()
-
-	if parsedURL, err = url.ParseRequestURI(requestURL); err != nil {
-		fmt.Printf("Help: ./http-get -h\nURL is not valid URL: %s\n", requestURL)
-		os.Exit(1)
-	}
-
-	client := http.Client{}
-
-	if password != "" {
-		token, err := doLoginRequest(client, parsedURL.Scheme+"://"+parsedURL.Host+"/login", password)
-		if err != nil {
-			if requestErr, ok := err.(RequestError); ok {
-				fmt.Printf("Login failed: %s (HTTP Error: %d, Body: %s)\n", requestErr.Error(), requestErr.HTTPCode, requestErr.Body)
-				os.Exit(1)
-			}
-			fmt.Printf("Login failed: %s\n", err)
-			os.Exit(1)
-		}
-		client.Transport = MyJWTTransport{
-			transport: http.DefaultTransport,
-			token:     token,
-		}
-	}
-
-	res, err := doRequest(client, parsedURL.String())
-	if err != nil {
-		if requestErr, ok := err.(RequestError); ok {
-			fmt.Printf("Error occurred: %s (HTTP Error: %d, Body: %s)\n", requestErr.Error(), requestErr.HTTPCode, requestErr.Body)
-			os.Exit(1)
-		}
-		fmt.Printf("Error occurred: %s\n", err)
-		os.Exit(1)
-	}
-	if res == nil {
-		fmt.Printf("No response\n")
-		os.Exit(1)
-	}
-	fmt.Printf("Response: %s\n", res.GetResponse())
 }
 
 func doRequest(client http.Client, requestURL string) (Response, error) {
